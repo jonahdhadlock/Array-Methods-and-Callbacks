98b02766790c1fde806c315253a3666dfd8c9609
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-04 17:14:34 -0600

    Rob Lark, my mentor, just helped me fix the main issue up front so I can actually do the assignment

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-import { fifaData } from './fifa.js';
-console.log(fifaData);
+const fifaData = require("./fifa.js");
 
 console.log('its working');
-// ⚽️ M  V P ⚽️ //
+
+// // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
-
+// (a) Home Team name for 2014 world cup final
+// */
+console.log();
+// /*
+// (b) Away Team name for 2014 world cup final
+// */
+console.log();
+// /*
+// (c) Home Team goals for 2014 world cup final
+// */
+console.log();
+// /*
+// (d) Away Team goals for 2014 world cup final
+// */
+console.log();
+// /*
+// (e) Winner of 2014 world cup final 
+// */
+console.log();
+/*
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
+function getFinals(data) {
+    const finals = [];
+    finals.push(fifaData);
+    return finals;
 
 };
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears( /* code here */ ) {
 
     /* code here */
 
@@ -31,9 +46,9 @@ function getYears(/* code here */) {
 
 getYears();
 
-/* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
+/* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */
 
-function getWinners(/* code here */) {
+function getWinners( /* code here */ ) {
 
     /* code here */
 
@@ -48,7 +63,7 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear(/* code here */) {
+function getWinnersByYear( /* code here */ ) {
 
 };
 
@@ -56,49 +71,10 @@ getWinnersByYear();
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals(/* code here */) {
+function getAverageGoals( /* code here */ ) {
 
     /* code here */
 
 };
 
 getAverageGoals();
-
-/// STRETCH 🥅 //
-
-/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
-
-Hint: Investigate your data to find "team initials"!
-Hint: use `.reduce` */
-
-function getCountryWins(/* code here */) {
-
-    /* code here */
-
-};
-
-getCountryWins();
-
-
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getGoals();
-
-
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
-
-function badDefense(/* code here */) {
-
-    /* code here */
-
-};
-
-badDefense();
-
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
