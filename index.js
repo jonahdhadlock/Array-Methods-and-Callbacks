ff62ab5cf0b96d48566a384b62157d0f69d9dd71
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-09-04 19:14:23 -0600

    completed all of task1; working on task2

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const fifaData = require("./fifa.js");
 
-console.log('its working');
+// console.log('its working');
 
 // // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
-
-// (a) Home Team name for 2014 world cup final
-// */
-console.log(fifaData["Home Team Name"]);
+/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data */
+const final2014 = fifaData.filter((game) => {
+        return game.Stage === "Final" && game.Year === 2014;
+    })
+    // (a) Home Team name for 2014 world cup final
+    // */
+console.log(final2014[0]["Home Team Name"]);
 // /*
 // (b) Away Team name for 2014 world cup final
 // */
-console.log();
+console.log(final2014[0]["Away Team Name"]);
 // /*
 // (c) Home Team goals for 2014 world cup final
 // */
-console.log();
+console.log(final2014[0]["Home Team Goals"]);
 // /*
 // (d) Away Team goals for 2014 world cup final
 // */
-console.log();
+console.log(final2014[0]["Away Team Goals"]);
 // /*
 // (e) Winner of 2014 world cup final 
 // */
-console.log();
+console.log(final2014[0]["Win conditions"]);
 /*
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+const finalGames = [];
 
 function getFinals(data) {
-    for (let i = 0; i < fifaData.length; i++) {
-        const finals = [];
-        finals.push(fifaData[i].stage);
-    }
-    return finals;
-};
-
+    const finalsData = fifaData.filter((data) => {
+        finalGames.push(data.Stage === "Final");
+    })
+    return finalGames;
+}
+console.log(getFinals(finalGames));
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
 function getYears(callback) {
